df7f93e4e6cfd575293bb4311c654f7f5510293d
Gaukhar <gaukharsabyrova@gmail.com>
2022-11-06 12:01:52 +0300

    Sort

---
+++ src/lessons/Sort.java
@@ -0,0 +1,38 @@
+package lessons;
+
+public class Sort {
+
+    public static int[] bubbleSort(int[] array) {
+        boolean isSorted = false;
+
+        while (!isSorted) {
+
+            isSorted = true;
+
+            for (int i = 1; i < array.length; i ++)  {
+                if (array[i - 1] > array[i]) {
+                    int temp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temp;
+
+                    isSorted = false;
+                }
+            }
+        }
+
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{64, 42, 73, 41, 32, 53, 16, 24, 57, 48, 74, 55, 36};
+
+        long start = 0;
+        long end = 0;
+        start = System.nanoTime();
+        bubbleSort(array);
+        end = System.nanoTime();
+
+        System.out.println(end - start);
+    }
+}
+
